9104ee3261e7da7731f9fbe466f518e08fb3a840
Rodrigo Erthal <rodrigofce@al.insper.edu.br>
2024-10-19 15:22:49 -0300

    Pergunta 5

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,42 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+   
+    posicoes = []
+    
+    while tamanho > 0:
+        if orientacao == "vertical":
+            posicoes.append([linha, coluna])
+            linha += 1  
+        elif orientacao == "horizontal":
+            posicoes.append([linha, coluna])
+            coluna += 1  
+        
+        tamanho -= 1  
+    
+    return posicoes 
+
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    
+    posicoes_navio = define_posicoes(linha, coluna, orientacao, tamanho)
+    
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes_navio)
+    else:
+        frota[nome_navio] = [posicoes_navio]
+    
+    return frota
+
+
+def faz_jogada(tabuleiro, linha, coluna):
+   
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'  
+    else:
+        tabuleiro[linha][coluna] = '-'  
+    
+    return tabuleiro
+
+
 def posiciona_frota(frota):
     
     tabuleiro = []
@@ -15,3 +54,22 @@ def posiciona_frota(frota):
                 tabuleiro[linha][coluna] = 1 
     
     return tabuleiro
+
+
+def afundados(frota, tabuleiro):
+    navios_afundados = 0
+    
+    for navio, posicoes in frota.items():
+        for lista_posicoes in posicoes:
+            afundado = 1  
+            
+            
+            for pos in lista_posicoes:
+                linha, coluna = pos
+                if tabuleiro[linha][coluna] != 'X':
+                    afundado = 0
+                    break
+        
+            navios_afundados += afundado
+                
+    return navios_afundados
